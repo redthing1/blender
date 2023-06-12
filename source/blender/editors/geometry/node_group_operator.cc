af43878c8e6796c910a2189ecc0a21e0a41e21dd
Hans Goudey <h.goudey@me.com>
2023-06-12 14:16:21 -0400

    Fixes

--- source/blender/editors/geometry/node_group_operator.cc
+++ source/blender/editors/geometry/node_group_operator.cc
@@ -19,8 +19,10 @@
 #include "BKE_geometry_set.hh"
 #include "BKE_layer.h"
 #include "BKE_lib_id.h"
+#include "BKE_material.h"
 #include "BKE_node_runtime.hh"
 #include "BKE_object.h"
+#include "BKE_pointcloud.h"
 #include "BKE_report.h"
 
 #include "DNA_object_types.h"
@@ -32,7 +34,7 @@
 #include "RNA_access.h"
 #include "RNA_define.h"
 #include "RNA_enum_types.h"
-#include "RNA_prototypes.h"
+// #include "RNA_prototypes.h"
 
 #include "UI_interface.h"
 #include "UI_resources.h"
@@ -42,30 +44,115 @@
 #include "NOD_geometry_nodes_execute.hh"
 #include "NOD_geometry_nodes_lazy_function.hh"
 
-#include "curves_intern.hh"
+#include "geometry_intern.hh"
 
-namespace blender::ed::curves {
+namespace blender::ed::geometry {
+
+static const bNodeTree *get_node_tree(Main &bmain, PointerRNA &ptr)
+{
+  char name[MAX_ID_NAME];
+  RNA_string_get(&ptr, "name", name);
+  const ID *id = BKE_libblock_find_name(bmain, ID_NT, name);
+  bNodeTree *node_tree = reinterpret_cast<const bNodeTree *>(id);
+  if (!node_tree) {
+    return nullptr;
+  }
+  if (node_tree->type != NTREE_GEOMETRY) {
+    return nullptr;
+  }
+  return node_tree;
+}
+
+class OperatorComputeContext : public ComputeContext {
+ private:
+  static constexpr const char *s_static_type = "OPERATOR";
+
+  std::string operator_name_;
+
+ public:
+  OperatorComputeContext(std::string operator_name)
+  {
+    hash_.mix_in(s_static_type, strlen(s_static_type));
+    hash_.mix_in(operator_name_.data(), operator_name_.size());
+  }
+
+ private:
+  void print_current_in_line(std::ostream &stream) const override
+  {
+    stream << "Operator: " << operator_name_;
+  }
+};
+
+static GeometrySet get_original_geometry_eval_copy(const Object &object)
+{
+  switch (object.type) {
+    case OB_CURVES: {
+      Curves *curves = BKE_curves_copy_for_eval(static_cast<const Curves *>(object.data));
+      return GeometrySet::create_with_curves(curves);
+    }
+    case OB_POINTCLOUD: {
+      PointCloud *points = BKE_pointcloud_copy_for_eval(
+          static_cast<const PointCloud *>(object.data));
+      return GeometrySet::create_with_pointcloud(points);
+    }
+    default:
+      return {};
+  }
+}
+
+static void store_result_geometry(Main &bmain, Object &object, GeometrySet geometry)
+{
+  switch (object.type) {
+    case OB_CURVES: {
+      Curves &curves = *static_cast<Curves *>(object.data);
+      Curves *new_curves = geometry.get_curves_for_write();
+      if (!new_curves) {
+        curves.geometry.wrap() = {};
+        break;
+      }
+
+      /* Anonymous attributes shouldn't be available on the applied geometry. */
+      new_curves->geometry.wrap().attributes_for_write().remove_anonymous();
+
+      curves.geometry.wrap() = std::move(new_curves->geometry.wrap());
+      BKE_object_material_from_eval_data(&bmain, &object, &new_curves->id);
+      break;
+    }
+    case OB_POINTCLOUD: {
+      PointCloud &points = *static_cast<PointCloud *>(object.data);
+      PointCloud *new_points = geometry.get_pointcloud_for_write();
+      if (!new_points) {
+        CustomData_free(&points.pdata, points.totpoint);
+        points.totpoint = 0;
+        break;
+      }
+
+      /* Anonymous attributes shouldn't be available on the applied geometry. */
+      new_points->attributes_for_write().remove_anonymous();
+
+      BKE_object_material_from_eval_data(&bmain, &object, &new_points->id);
+      BKE_pointcloud_nomain_to_pointcloud(new_points, &points);
+      break;
+    }
+  }
+}
 
 static int run_node_group_exec(bContext *C, wmOperator *op)
 {
+  Main *bmain = CTX_data_main(C);
   Depsgraph *depsgraph = CTX_data_ensure_evaluated_depsgraph(C);
   Scene *scene = CTX_data_scene(C);
   ViewLayer *view_layer = CTX_data_view_layer(C);
-  Object *object = CTX_data_active_object(C);
-  if (!object) {
-    return OPERATOR_CANCELLED;
-  }
-  eObjectMode mode = eObjectMode(object->mode);
+  Object *active_object = CTX_data_active_object(C);
+  eObjectMode mode = active_object ? eObjectMode(object->mode) : OB_MODE_OBJECT;
 
-  char name[MAX_ID_NAME];
-  RNA_string_get(op->ptr, "name", name);
-  const ID *id = BKE_libblock_find_name(CTX_data_main(C), ID_NT, name);
-  if (!id) {
-    return OPERATOR_CANCELLED;
+  const bNodeTree *node_tree = get_node_tree(*bmain, *op->ptr);
+  if (!node_tree) {
+    return;
   }
-  const bNodeTree &node_tree = reinterpret_cast<const bNodeTree &>(*id);
+
   const nodes::GeometryNodesLazyFunctionGraphInfo *lf_graph_info =
-      nodes::ensure_geometry_nodes_lazy_function_graph(node_tree);
+      nodes::ensure_geometry_nodes_lazy_function_graph(*node_tree);
   if (lf_graph_info == nullptr) {
     BKE_report(op->reports, RPT_ERROR, "Cannot evaluate node group");
     return OPERATOR_CANCELLED;
@@ -75,37 +162,32 @@ static int run_node_group_exec(bContext *C, wmOperator *op)
   Object **objects = BKE_view_layer_array_from_objects_in_mode_unique_data(
       scene, view_layer, CTX_wm_view3d(C), &objects_len, mode);
 
-  for (Object *object : Span(objects, objects_len)) {
-    Curves &curves_id = *static_cast<Curves *>(object->data);
-    GeometrySet original_geometry = GeometrySet::create_with_curves(
-        &curves_id, GeometryOwnershipType::Editable);
+  OperatorComputeContext compute_context(op->type->idname);
 
+  for (Object *object : Span(objects, objects_len)) {
+    if (!ELEM(object->type, OB_CURVES, OB_POINTCLOUD)) {
+      continue;
+    }
     nodes::GeoNodesOperatorData operator_eval_data{};
     operator_eval_data.depsgraph = depsgraph;
     operator_eval_data.self_object = object;
 
-    bke::ModifierComputeContext compute_context{nullptr, "actually not a modifier"};
-    GeometrySet new_geometry = nodes::execute_geometry_nodes(
-        node_tree,
+    GeometrySet geometry_orig = get_original_geometry_eval_copy(*object);
+
+    GeometrySet new_geometry = nodes::execute_geometry_nodes_on_geometry(
+        *node_tree,
         op->properties,
         compute_context,
-        original_geometry,
+        geometry_orig,
         [&](nodes::GeoNodesLFUserData &user_data) {
           user_data.operator_data = &operator_eval_data;
           user_data.log_socket_values = false;
         });
 
-    if (Curves *new_curves_id = new_geometry.get_curves_for_write()) {
-      if (new_curves_id != &curves_id) {
-        curves_id.geometry.wrap() = std::move(new_curves_id->geometry.wrap());
-      }
-    }
-    else {
-      curves_id.geometry.wrap() = {};
-    }
+    store_result_geometry(*bmain, *object, std::move(new_geometry));
 
-    DEG_id_tag_update(&curves_id.id, ID_RECALC_GEOMETRY);
-    WM_event_add_notifier(C, NC_GEOM | ND_DATA, &curves_id);
+    DEG_id_tag_update(static_cast<ID *>(object->data), ID_RECALC_GEOMETRY);
+    WM_event_add_notifier(C, NC_GEOM | ND_DATA, object->data);
   }
 
   MEM_SAFE_FREE(objects);
@@ -115,29 +197,35 @@ static int run_node_group_exec(bContext *C, wmOperator *op)
 
 static int run_node_group_invoke(bContext *C, wmOperator *op, const wmEvent *event)
 {
-  char name[MAX_ID_NAME];
-  RNA_string_get(op->ptr, "name", name);
-  const ID *id = BKE_libblock_find_name(CTX_data_main(C), ID_NT, name);
-  if (!id) {
+  const bNodeTree *node_tree = get_node_tree(*CTX_data_main(C), *op->ptr);
+  if (!node_tree) {
     return OPERATOR_CANCELLED;
   }
-  const bNodeTree &node_tree = reinterpret_cast<const bNodeTree &>(*id);
 
-  nodes::update_input_properties_from_node_tree(node_tree, op->properties, *op->properties);
-  nodes::update_output_properties_from_node_tree(node_tree, op->properties, *op->properties);
+  nodes::update_input_properties_from_node_tree(*node_tree, op->properties, *op->properties);
+  nodes::update_output_properties_from_node_tree(*node_tree, op->properties, *op->properties);
 
   return run_node_group_exec(C, op);
 }
 
-void CURVES_OT_node_group(wmOperatorType *ot)
+static char *run_node_group_get_description(bContext *C, wmOperatorType * /*ot*/, PointerRNA *ptr)
+{
+  const bNodeTree *node_tree = get_node_tree(*CTX_data_main(C), *ptr);
+  if (!node_tree) {
+    return;
+  }
+  return nullptr;
+}
+
+void GEOMETRY_OT_execute_node_group(wmOperatorType *ot)
 {
   ot->name = "Run Node Group";
   ot->idname = __func__;
-  ot->description = "Execute a node group on curves";  // TODO: Retrieve from node group.
+  ot->description = "Execute a node group on geometry";
 
   ot->invoke = run_node_group_invoke;
   ot->exec = run_node_group_exec;
-  ot->poll = editable_curves_poll;
+  ot->get_description = run_node_group_get_description;
 
   ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
 
@@ -150,4 +238,4 @@ void CURVES_OT_node_group(wmOperatorType *ot)
   RNA_def_property_flag(prop, PropertyFlag(PROP_SKIP_SAVE | PROP_HIDDEN));
 }
 
-}  // namespace blender::ed::curves
+}  // namespace blender::ed::geometry
